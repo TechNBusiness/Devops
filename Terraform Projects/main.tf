87811a1430d7a8dd1cf1411af765282d64357caf
TechNBusiness <97891437+TechNBusiness@users.noreply.github.com>
2022-07-05 20:57:28 +0100

    Update main.tf

--- Terraform Projects/main.tf
+++ Terraform Projects/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "eu-east-1"
-  access_key = "AKIATTEXR3LFCHCZ3JWF"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 resource "ec21" {
   
