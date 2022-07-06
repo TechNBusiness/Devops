9ac93cf8ed55a7becb12fd573e9543d705df3532
Chukwuebuka Aduebe <Chukwuebuka.aduebe@globalaccelerex.com>
2022-07-06 14:04:40 +0100

    "Fifth Commit"

--- Terraform Projects/main.tf
+++ Terraform Projects/main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
-  region = "eu-east-1"
+  region = "us-east-1"
   access_key = ""
   secret_key = ""
 }
-resource "ec21" {
-  
+resource "aws_vpc" "terraform-vpc" {
+    cidr_block = "10.0.0.0/16"
+} 
+
+resource "aws_subnet" "terraform-subnet-1" {
+   vpc_id = aws_vpc.terraform-vpc.id
+   cidr_block = "10.0.11.0/24"
+   availability_zone = "us-east-1a"
 }
+
